2224bd5b8b22c1c54b9d54069d22d4c0e923b6d6
brunoSacchetti <sacchettibruno03@gmail.com>
2024-09-05 18:20:23 -0300

    resolucion

--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,28 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection } from './models';
+import * as minimist from 'minimist'
+
+''
 
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
+  const resultado = minimist(argv);
+  console.log(resultado);
+  
   return {
-    action: null,
-    params: null,
+    action: resultado.action,
+    params: JSON.parse(resultado.params)
   };
 }
 
-function main() {}
+// ts-node source/index.ts --action save --params '{id:1, nombre:"Brunito"}' ejemplo de ejecucion
+function main() {
+  const controller = new ContactsController();
+  const params = parseaParams(process.argv.slice(2));
+  const resultado = controller.processOptions(params);
+  console.log(resultado);
+  
+
+}
 
 main();
